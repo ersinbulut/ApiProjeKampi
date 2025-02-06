cd5bbe358daedf060d4bd70cd5676d1f773022c0
Developer <Developer@DEV>
2025-02-06 14:01:24 +0300

    #3 Api Proje Kampı - Entitylerin Oluşturulması

---
+++ ApiProjeKampi.WebApi/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
