2f849c5ba4bcb58835ef3ca23f2b8f0c49b3b24c
Developer <Developer@DEV>
2025-02-07 08:12:53 +0300

    #4 Api Proje Kampı - Entitylerin Tamamlanması ve Paketlerin Kurulması

---
+++ ApiProjeKampi.WebApi/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Image
+    {
+        public int ImageID { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
