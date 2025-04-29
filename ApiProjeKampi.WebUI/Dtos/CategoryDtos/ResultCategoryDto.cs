c4ecb8ef3cefe071d22fe813333b26c84fc808f9
Developer <Developer@DEV>
2025-04-29 13:56:26 +0300

    #26 Api Proje Kampı - Menünün UI'da Listelenmesi

---
+++ ApiProjeKampi.WebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiProjeKampi.WebUI.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentPartial.cs
@@ -0,0 +1,34 @@
+﻿using ApiProjeKampi.WebUI.Dtos.CategoryDtos;
+using ApiProjeKampi.WebUI.Dtos.ProductDtos;
+using ApiProjeKampi.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuProductComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuProductComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7096/api/Products");
+
+            if (responseMessage != null && responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+
+            // Eğer null ise veya başarısızsa boş modelle döner
+            return View(new List<ResultServiceDto>());
+        }
+    }
+}
