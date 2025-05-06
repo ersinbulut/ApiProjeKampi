9406ed0b9c3c0718e304eb21cb9544da5c98715c
Developer <Developer@DEV>
2025-05-06 10:51:03 +0300

    #27 Api Proje Kampı - Testimonial Api ve Consume

---
+++ ApiProjeKampi.WebUI/Dtos/TestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebUI.Dtos.TestimonialDtos
+{
+    public class ResultTestimonialDto
+    {
+        public int TestimonialID { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
