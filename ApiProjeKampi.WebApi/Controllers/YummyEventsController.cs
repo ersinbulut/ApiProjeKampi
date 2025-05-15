80d379bb7537e699606004d280608e56c15314dc
Developer <Developer@DEV>
2025-05-15 12:12:23 +0300

    #28 Api Proje Kampı - Events Api

---
+++ ApiProjeKampi.WebApi/Controllers/YummyEventsController.cs
@@ -0,0 +1,58 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YummyEventsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public YummyEventsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult YummyEventList()
+        {
+            var values = _context.YummyEvents.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateYummyEvent(YummyEvent yummyEvent)
+        {
+            _context.YummyEvents.Add(yummyEvent);
+            _context.SaveChanges();
+            return Ok("Kategori Ekleme İşlemi Başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+            _context.YummyEvents.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori Silme İşlemi Başarılı");
+        }
+
+        [HttpGet("GetYummyEvent")]
+        public IActionResult GetYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateYummyEvent(YummyEvent yummyEvent)
+        {
+            _context.YummyEvents.Update(yummyEvent);
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme işlemi başarılı");
+        }
+    }
+}
---
+++ ApiProjeKampi.WebUI/ViewComponents/_EventDefaultComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using ApiProjeKampi.WebUI.Dtos.EventDtos;
+using ApiProjeKampi.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _EventDefaultComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _EventDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7096/api/YummyEvents");
+
+            if (responseMessage != null && responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultEventDto>>(jsonData);
+                return View(values);
+            }
+
+            // Eğer null ise veya başarısızsa boş modelle döner
+            return View(new List<ResultEventDto>());
+        }
+    }
+}
