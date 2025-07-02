c3242ed2a5e7827d1eb2b97b95ede99846034f31
Developer <Developer@DEV>
2025-07-02 17:14:02 +0300

    #44 Api Proje Kampı - Admin Panelinde Feature İşlemleri

--- ApiProjeKampi.WebUI/Controllers/ProductController.cs
+++ ApiProjeKampi.WebUI/Controllers/ProductController.cs
@@ -19,7 +19,7 @@ namespace ApiProjeKampi.WebUI.Controllers
         public async Task<IActionResult> ProductList()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7096/api/Products");
+            var responseMessage = await client.GetAsync("https://localhost:7096/api/Products/ProductListWithCategory");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
