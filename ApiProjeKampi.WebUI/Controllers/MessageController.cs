1c4cbe7d8288e6f6b005d71000e729bc3f6b0a04
Developer <Developer@DEV>
2025-08-15 15:28:20 +0300

    56..

---
+++ ApiProjeKampi.WebUI/Controllers/MessageController.cs
@@ -0,0 +1,149 @@
+﻿using ApiProjeKampi.WebUI.Dtos.MessageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using static ApiProjeKampi.WebUI.Controllers.AIController;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MessageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MessageList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7020/api/Messages");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMessageDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createMessageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7020/api/Messages", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("MessageList");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7020/api/Messages?id=" + id);
+            return RedirectToAction("MessageList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateMessage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7020/api/Messages/GetMessage?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<GetMessageByIdDto>(jsonData);
+            return View(value);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateMessageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client.PutAsync("https://localhost:7020/api/Messages/", stringContent);
+            return RedirectToAction("MessageList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AsnwerMessageWithOpenAI(int id, string prompt)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7020/api/Messages/GetMessage?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<GetMessageByIdDto>(jsonData);
+            prompt = value.MessageDetails;
+
+            var apiKey = "";
+
+            using var client2 = new HttpClient();
+            client2.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            var requestData = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new
+                    {
+                        role="system",
+                        content="Sen bir restoran için kullanıcıların göndermiş oldukları mesajları detaylı ve olabildiğince olumlu, müşteri memnunyeti gözeten cevaplar veren bir yapay zeka aracısın. Amacımız kullanıcı tarafından gönderilen mesajlara en olumlu ve mantıklı cevapları sunabilmek."
+                    },
+                    new
+                    {
+                        role="user",
+                        content= prompt
+                    }
+                },
+                temperature = 0.5
+            };
+
+            var response = await client2.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestData);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<OpenAIResponse>();
+                var content = result.choices[0].message.content;
+                ViewBag.answerAI = content;
+            }
+            else
+            {
+                ViewBag.answerAI = "Bir hata oluştu: " + response.StatusCode;
+            }
+
+
+
+            return View(value);
+        }
+
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createMessageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7020/api/Messages", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("MessageList");
+            }
+            return View();
+        }
+    }
+}
